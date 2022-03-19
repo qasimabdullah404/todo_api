9cc0e6cffaaadf9943f5e6651c4c303cb846f3dd
Qasim Abdullah <qasimabdullah404@gmail.com>
2022-03-19 13:48:18 +0500

    Todo CRUD

--- app/controllers/api/v1/todos_controller.rb
+++ app/controllers/api/v1/todos_controller.rb
@@ -3,4 +3,48 @@ class Api::V1::TodosController < ApplicationController
     todos = Todo.all
     render json: TodoSerializer.new(todos), status: :ok
   end
+
+  def create
+    todo = Todo.new(todo_params)
+    if todo.save
+      render json: TodoSerializer.new(todo), status: :created
+    else
+      render json: { errors: todo.errors.messages }, status: :unprocessable_entity
+    end
+  end
+
+  def show
+    begin
+      todo = Todo.find(params[:id])
+      render json: TodoSerializer.new(todo), status: :ok
+    rescue ActiveRecord::RecordNotFound
+      render json: { errors: "Todo task not found!" }, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    todo = Todo.find(params[:id])
+    if todo.update(todo_params)
+      render json: TodoSerializer.new(todo), status: :ok
+    else
+      render json: { errors: todo.errors.messages }, status: :unprocessable_entity
+    end
+  rescue ActiveRecord::RecordNotFound
+    render json: { errors: "Todo task not found!" }, status: :unprocessable_entity
+  end
+
+  def destroy
+    todo = Todo.find(params[:id])
+    if todo.destroy
+      head :no_content
+    else
+      render json: { errors: "An error occured, couldn't delete todo." }, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def todo_params
+    params.require(:todo).permit(:task, :task_completed)
+  end
 end
