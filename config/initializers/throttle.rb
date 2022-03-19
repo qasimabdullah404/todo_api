b1daab8e0fbb00cf3cfe408ff3a645a434b58f46
Qasim Abdullah <qasimabdullah404@gmail.com>
2022-03-19 14:27:06 +0500

    allow 60 requests in 15 minutes

--- config/initializers/throttle.rb
+++ config/initializers/throttle.rb
@@ -3,5 +3,5 @@ require 'redis'
 redis_config = YAML.load(File.join(Rails.root, "config", "redis.yml"))
 REDIS = Redis.new(host: redis_config["host"], port: redis_config["port"])
 
-THROTTLE_TIME_WINDOW = 15 * 50
-THROTTLE_MAX_REQUESTS = 50
+THROTTLE_TIME_WINDOW = 15 * 60
+THROTTLE_MAX_REQUESTS = 60
