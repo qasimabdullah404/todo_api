0d695d0e3cd92e2c6d93c3d25f42fe4cbb32fdca
Qasim Abdullah <qasimabdullah404@gmail.com>
2022-03-19 13:47:58 +0500

    update validations

--- app/models/todo.rb
+++ app/models/todo.rb
@@ -1,3 +1,4 @@
 class Todo < ApplicationRecord
-  validates_presence_of :task, on: :create, message: "Task can't be blank"
+  validates_presence_of :task, message: "can't be blank"
+  validates_length_of :task, within: 5..140, message: "must be between 5 and 140 characters."
 end
