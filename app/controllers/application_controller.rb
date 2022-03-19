c0a3de8b8751058e1d8104fc3d4c60433180a759
Qasim Abdullah <qasimabdullah404@gmail.com>
2022-03-19 14:18:34 +0500

    add requests limiting logic

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,19 @@
 class ApplicationController < ActionController::API
+  before_action :throttle
+
+  def throttle
+    client_ip = request.env["REMOTE_ADDR"]
+    key = "count:#{client_ip}"
+    count = REDIS.get(key)
+
+    unless count
+      REDIS.set(key, 0)
+      REDIS.expire(key, THROTTLE_TIME_WINDOW)
+      return true
+    end
+
+    render json: {"message":"You have reached the requests limit. Please wait for some time."}, status: 429 if count.to_i >= THROTTLE_MAX_REQUESTS
+    REDIS.incr(key)
+    true
+  end
 end
